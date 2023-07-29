9446aba3df7c38a37daa9f35bb7733f277e7dec7
Jaden Lee <jaden.lee101@gmail.com>
2023-07-29 17:58:34 -0600

    Initial Commit

---
+++ CPO Swap/CPO_SwapApp.swift
@@ -0,0 +1,17 @@
+//
+//  CPO_SwapApp.swift
+//  CPO Swap
+//
+//  Created by Jaden Lee on 2023-07-29.
+//
+
+import SwiftUI
+
+@main
+struct CPO_SwapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
